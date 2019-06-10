f2df0e3d3b1b224c5882630d71a9b7b36936e23c
RenuPandey <38316571+RenuPandeyPJI@users.noreply.github.com>
2019-06-10 15:22:33 +0530

    first commit

--- src/test/java/com/example/springsocial/SpringSocialApplicationTests.java
+++ src/test/java/com/example/springsocial/SpringSocialApplicationTests.java
@@ -1,14 +1,12 @@
-package com.example.springsocial;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-public class SpringSocialApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+/*
+ * package com.example.springsocial;
+ * 
+ * import org.junit.Test; import org.junit.runner.RunWith; import
+ * org.springframework.test.context.junit4.SpringRunner;
+ * 
+ * @RunWith(SpringRunner.class) public class SpringSocialApplicationTests {
+ * 
+ * @Test public void contextLoads() { }
+ * 
+ * }
+ */
